36e673e926f72f4cb83682fcbab9c7a7d0392a5d
xeptore <29199390+xeptore@users.noreply.github.com>
2026-01-06 11:32:07 +0000

    refactor: improve code readability and formatting in attach-file and mention-customization plugins
    Signed-off-by: xeptore <29199390+xeptore@users.noreply.github.com>

--- packages/ckeditor5-build-classic/plugins/attach-file.js
+++ packages/ckeditor5-build-classic/plugins/attach-file.js
@@ -2,62 +2,61 @@ import Plugin from '@ckeditor/ckeditor5-core/src/plugin';
 import FileRepository from '@ckeditor/ckeditor5-upload/src/filerepository';
 
 export default class AttachFile extends Plugin {
-  init() {
-    const editor = this.editor;
-    const imageTypes = editor.config.get('image.upload.types').map((type) => `image/${type}`);
+	init() {
+		const editor = this.editor;
+		const imageTypes = editor.config.get( 'image.upload.types' ).map( type => `image/${ type }` );
 
-    this.listenTo(editor.editing.view.document, 'clipboardInput', (evt, data) => {
-      const files = Array.from(data.dataTransfer.files).filter((file) => {
-        if (!file) {
-          return false;
-        }
+		this.listenTo( editor.editing.view.document, 'clipboardInput', ( evt, data ) => {
+			const files = Array.from( data.dataTransfer.files ).filter( file => {
+				if ( !file ) {
+					return false;
+				}
 
-        return !imageTypes.includes(file.type);
-      });
+				return !imageTypes.includes( file.type );
+			} );
 
-	  if (!data.targetRanges) {
-		  return
-	  }
+			if ( !data.targetRanges ) {
+				return;
+			}
 
-      const ranges = data.targetRanges.map((viewRange) => editor.editing.mapper.toModelRange(viewRange));
+			const ranges = data.targetRanges.map( viewRange => editor.editing.mapper.toModelRange( viewRange ) );
 
-      editor.model.change((writerModel) => {
-        writerModel.setSelection(ranges);
+			editor.model.change( writerModel => {
+				writerModel.setSelection( ranges );
 
-        if (files.length) {
-          evt.stop();
+				if ( files.length ) {
+					evt.stop();
 
-          editor.model.enqueueChange('default', () => {
-            const fileRepository = editor.plugins.get(FileRepository);
+					editor.model.enqueueChange( 'default', () => {
+						const fileRepository = editor.plugins.get( FileRepository );
 
-            files.forEach((file) => {
-              const loader = fileRepository.createLoader(file);
+						files.forEach( file => {
+							const loader = fileRepository.createLoader( file );
 
-              if (!loader) {
-                return;
-              }
+							if ( !loader ) {
+								return;
+							}
 
-              loader.upload().then((result) => {
-                editor.model.change((writer) => {
-                  const node = writer.createText(result.text, {
-                    linkHref: result.default,
-                  });
+							loader.upload().then( result => {
+								editor.model.change( writer => {
+									const node = writer.createText( result.text, {
+										linkHref: result.default
+									} );
 
-                  const position = editor.model.document.selection.getFirstPosition();
+									const position = editor.model.document.selection.getFirstPosition();
 
-                  editor.model.insertContent(node, position);
+									editor.model.insertContent( node, position );
 
-                  const emptySpace = writer.createText(' ');
-                  editor.model.insertContent(emptySpace, writer.createPositionAfter(node));
+									const emptySpace = writer.createText( ' ' );
+									editor.model.insertContent( emptySpace, writer.createPositionAfter( node ) );
 
-                  writer.setSelection(writer.createPositionAfter(emptySpace));
-                });
-              });
-            });
-
-          });
-        }
-      });
-    });
-  }
+									writer.setSelection( writer.createPositionAfter( emptySpace ) );
+								} );
+							} );
+						} );
+					} );
+				}
+			} );
+		} );
+	}
 }
--- packages/ckeditor5-build-classic/plugins/mention-customization.js
+++ packages/ckeditor5-build-classic/plugins/mention-customization.js
@@ -2,75 +2,75 @@ import Plugin from '@ckeditor/ckeditor5-core/src/plugin';
 import MouseObserver from '@ckeditor/ckeditor5-engine/src/view/observer/mouseobserver';
 
 export default class MentionCustomization extends Plugin {
-  init() {
-    const editor = this.editor;
-    const viewDocument = this.editor.editing.view.document;
+	init() {
+		const editor = this.editor;
+		const viewDocument = this.editor.editing.view.document;
 
-    editor.editing.view.addObserver(MouseObserver);
+		editor.editing.view.addObserver( MouseObserver );
 
-    editor.conversion.for('upcast').elementToAttribute({
-      view: {
-        name: 'a',
-        key: 'data-mention',
-        classes: 'mention',
-        attributes: {
-          href: true,
-        },
-      },
-      model: {
-        key: 'mention',
-        value: (viewItem) => {
-          const mentionAttribute = editor.plugins.get('Mention').toMentionAttribute(viewItem, {
-            link: viewItem.getAttribute('href'),
-          });
+		editor.conversion.for( 'upcast' ).elementToAttribute( {
+			view: {
+				name: 'a',
+				key: 'data-mention',
+				classes: 'mention',
+				attributes: {
+					href: true
+				}
+			},
+			model: {
+				key: 'mention',
+				value: viewItem => {
+					const mentionAttribute = editor.plugins.get( 'Mention' ).toMentionAttribute( viewItem, {
+						link: viewItem.getAttribute( 'href' )
+					} );
 
-          return mentionAttribute;
-        },
-      },
-      converterPriority: 'high',
-    });
+					return mentionAttribute;
+				}
+			},
+			converterPriority: 'high'
+		} );
 
-    editor.conversion.for('downcast').attributeToElement({
-      model: 'mention',
-      view: (modelAttributeValue, { writer }) => {
-        if (!modelAttributeValue || !modelAttributeValue.id) {
-          return;
-		    }
+		editor.conversion.for( 'downcast' ).attributeToElement( {
+			model: 'mention',
+			view: ( modelAttributeValue, { writer } ) => {
+				if ( !modelAttributeValue || !modelAttributeValue.id ) {
+					return;
+				}
 
-        if (modelAttributeValue.id.startsWith(':')) {
-          return writer.createAttributeElement('span', {
-            class: 'mention',
-          }, {
-            priority: 20,
-            id: modelAttributeValue.uid,
-          });
-        } else {
-          return writer.createAttributeElement('a', {
-            class: 'mention',
-            href: modelAttributeValue.link,
-            target: '_blank',
-          }, {
-            priority: 20,
-            id: modelAttributeValue.uid,
-          });
-        }
+				if ( modelAttributeValue.id.startsWith( ':' ) ) {
+					return writer.createAttributeElement( 'span', {
+						class: 'mention'
+					}, {
+						priority: 20,
+						id: modelAttributeValue.uid
+					} );
+				} else {
+					return writer.createAttributeElement( 'a', {
+						class: 'mention',
+						href: modelAttributeValue.link,
+						target: '_blank'
+					}, {
+						priority: 20,
+						id: modelAttributeValue.uid
+					} );
+				}
+			},
+			converterPriority: 'high'
+		} );
 
-      },
-      converterPriority: 'high',
-    });
+		this.listenTo( viewDocument, 'click', () => {
+			const selection = viewDocument.selection;
+			const position = selection.getFirstPosition();
+			const mention = position.getAncestors().find( ancestor => this.isMentionLink( ancestor ) );
 
-    this.listenTo(viewDocument, 'click', () => {
-      const selection = viewDocument.selection;
-      const position = selection.getFirstPosition();
-      const mention = position.getAncestors().find((ancestor) => this.isMentionLink(ancestor));
+			if ( mention ) {
+				// eslint-disable-next-line no-undef
+				window.open( mention.getAttribute( 'href' ) );
+			}
+		} );
+	}
 
-      if (mention) {
-        window.open(mention.getAttribute('href'));
-      }
-    });
-  }
-
-  isMentionLink(node) {
-    return node.is('attributeElement') && node.hasClass('mention') && node.getAttribute('href');
-  }
+	isMentionLink( node ) {
+		return node.is( 'attributeElement' ) && node.hasClass( 'mention' ) && node.getAttribute( 'href' );
+	}
 }
